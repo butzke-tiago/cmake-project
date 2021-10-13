648e78534cf3781ecabf14aa96edd5a95fd6344d
Tiago Butzke dos Santos <butzke.tiago@gmail.com>
2021-10-13 11:21:59 -0600

    Added a base project.

---
+++ code/main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Tiago Santos
+ * @brief
+ * @version 0.1
+ * @date 2021-10-13
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+/* System includes */
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    printf("CMake Project\n");
+}
